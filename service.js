5d7916f4734ef09b7f9728a115084e5419bdf298
SJ Miller <sj.miller.tn@gmail.com>
2023-11-10 00:51:51 -0500

    fixing some password validation errors

--- service.js
+++ service.js
@@ -1,19 +1,19 @@
+// Imports and setup
 const express = require('express');
 const { check, validationResult } = require('express-validator');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 var app = express();
 app.use(express.json());
-
 var router = express.Router();
-
 var biz = require('./business/business');
 
+
 // Validators/sanitizers with express-validator
-var usernameValidate = () => check('username').notEmpty().isLength({max: 45}).trim().escape();
-var passValidate = () => check('password').notEmpty().isLength({max: 45}).trim().escape();
+var usernameValidate = () => check('username').escape().notEmpty().isLength({ max: 45 }).trim();
+var passValidate = () => check('password').escape().notEmpty().isLength({ max: 45 }).trim();
+
 
 // API requests
 // Tester to make sure server is connected
@@ -27,35 +27,39 @@ router.post('/test', usernameValidate(), (req, res) => {
     }
     res.send({ errors: result.array() });
 });
+
 // Create account
 router.post('/newUser', usernameValidate(), passValidate(), async (req, res) => {
     // Checks that there were no validation errors
     const result = validationResult(req);
+    console.log(result);
     if (result.isEmpty()) {
         // If no validation errors, send to business to create user
         let result = await biz.newUser(req.body.username, req.body.password);
         return res.send(result);
     }
     // If errors, send message
-    res.send({error: 'Invalid username or password. Try again.'})
+    res.send({ error: 'Invalid username or password. Try again.' })
 })
+
 
 // Pages 
 // Create account
-app.get('/newUser', (req, res) =>{
+app.get('/newUser', (req, res) => {
     res.sendFile(path.join(__dirname, '/pages/login/newUser.html'));
 });
+
 // Log in
-app.get('/login', (req, res) =>{
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/pages/login/login.html'));
 });
 
+
+// Finishing setup
 app.use('/api', router);
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
 app.listen(5000, function () {
     console.log('Started application on port %d', 5000);
 });
